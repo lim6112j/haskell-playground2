a579559c86da0e4d66f95396a11f3529be18f8a6
lim6112j <lim6112j@gmail.com>
2022-06-25 18:34:23 +0900

    probability monad added

--- src/CustomMonad.hs
+++ src/CustomMonad.hs
@@ -1,6 +1,9 @@
+{-# OPTIONS_GHC -Wno-deferred-type-errors #-}
+
 -- |
 module CustomMonad (someFunc) where
 
+import Control.Applicative
 import Data.Functor
 import Data.Ratio
 
@@ -27,9 +30,43 @@ join m = do
 
 -- m >>= f  == join (fmap f m)
 
+newtype Prob a = Prob {getProb :: [(a, Rational)]} deriving (Show)
+
+flatten :: Prob (Prob a) -> Prob a
+flatten (Prob xs) = Prob $ concat $ map f xs
+  where
+    f (Prob inner, p) = map (\(x, r) -> (x, p * r)) inner
+
+instance Functor (Prob) where
+  fmap f (Prob xs) = Prob $ map (\(x, p) -> (f x, p)) xs
+
+instance Applicative (Prob) where
+  pure x = Prob [(x, 1 % 1)]
+  (<*>) = ap
+
+instance Monad (Prob) where
+  return x = Prob [(x, 1 % 1)]
+  p1 >>= p2 = flatten (fmap p2 p1)
+
+data Coin = Heads | Tails deriving (Show, Eq)
+
+coin :: Prob Coin
+coin = Prob [(Heads, 1 % 2), (Tails, 1 % 2)]
+
+loadedCoin :: Prob Coin
+loadedCoin = Prob [(Heads, 1 % 10), (Tails, 9 % 10)]
+
+flipThree :: Prob Bool
+flipThree = do
+  a <- coin
+  b <- coin
+  c <- loadedCoin
+  return (all (== Tails) [a, b, c])
+
 someFunc :: IO ()
 someFunc = do
   print $ liftM (+ 1) (Just 8)
   print $ ap (Just (+ 1)) (Just 4)
   print $ liftM (+ 1) [1, 2, 3, 4]
   print $ ap [(+ 1), (+ 2)] [1, 2, 3, 4]
+  print $ getProb flipThree
